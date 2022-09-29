ce18293201cd5dd31b09b1e4a240657a88b5a4b5
Ahmad Jabar Ilmi <5022211038@mhs.its.ac.id>
2022-09-29 22:42:50 +0700

    menambahkan variabel baru

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int data;
+int main()
+{
+    data = 10;
+    std::cout << data << std::endl;
+    data = 20;
+    std::cout << data << std::endl;
+    data = 30;
+    std::cout << data << std::endl;
+    data = 40;
+    std::cout << data << std::endl;
+
+    return 0;
+}
+
